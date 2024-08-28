ce5b155473e8dfba9101f891a70a6a810f245c4c
Pathogenbeats <0933490@my.scccd.edu>
2024-08-28 11:14:45 -0700

    Add files via upload
    This was the oddOrEven string we programmed with the if and else statements 8/28/24

---
+++ functionPractice.cpp
@@ -0,0 +1,43 @@
+// Andrew Powell 8/28/24
+// functionPractice.cpp
+
+#include <iostream>
+using namespace std;
+
+// Function Prototypes go here.
+// This is declaring a function.
+string oddOrEven(int);
+
+int main() {
+    cout << "\nWelcome to my Function Program!" << endl;
+
+    string oddOrEvenString = "";
+
+    int userInput = 0;
+    // Get input from the user.
+
+    cout << "\nPlease enter your number: ";
+    cin >> userInput;
+
+    // call the odd or even function.
+
+    oddOrEvenString = oddOrEven(userInput);
+    cout << "\nYour Input Number Was " << oddOrEvenString << endl;
+
+    return 0;
+}
+
+string oddOrEven(int num1) {
+    string myReturnStr = "";
+
+    if (num1 % 2 == 0) {
+        myReturnStr = "Even";
+        //num1 is even
+    }
+     else
+     {
+         myReturnStr = "Odd";
+         //this is the false statement
+     }
+    return myReturnStr;
+}
